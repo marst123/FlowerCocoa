52c522d7f69801cc2367c1e18cfec8b8835c377f
guanyi.wang <wangguanyi@xubei.com>
2022-07-06 14:16:24 +0800

    Initial Commit

---
+++ FlowerCocoa/FlowerCocoa.h
@@ -0,0 +1,18 @@
+//
+//  FlowerCocoa.h
+//  FlowerCocoa
+//
+//  Created by zhuxiang on 2022/7/6.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlowerCocoa.
+FOUNDATION_EXPORT double FlowerCocoaVersionNumber;
+
+//! Project version string for FlowerCocoa.
+FOUNDATION_EXPORT const unsigned char FlowerCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowerCocoa/PublicHeader.h>
+
+
